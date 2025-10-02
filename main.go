1dc67d9c2800b9b2ca831a1531c421436603815f
Youssef <youssefalaaeldinn@gmail.com>
2025-10-02 20:37:11 +0300

    Unix shell initial commit

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func execInput(input string) error {
+	// Remove new line char
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	// Prepare cmd
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("cd requires at least one argument")
+		}
+
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+        // Read the keyboad input.
+        input, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err)
+        }
+
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+    }
+}
